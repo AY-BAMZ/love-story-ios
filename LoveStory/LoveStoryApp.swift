c853c79c8841417471e126219559fc288fd23d6c
ayobami paul <aybamz181@gmail.com>
2024-06-30 21:27:57 +0100

    Initial Commit

---
+++ LoveStory/LoveStoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoveStoryApp.swift
+//  LoveStory
+//
+//  Created by MAC on 30/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct LoveStoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
